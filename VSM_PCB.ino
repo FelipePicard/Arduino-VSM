b47d9e6dd5ca9490ca99adaa2d8f8ed39e0695d6
Felipe Picard <44078452+FelipePicard@users.noreply.github.com>
2023-03-23 17:44:23 -0300

    updating cup check

--- VSM_PCB.ino
+++ VSM_PCB.ino
@@ -1,13 +1,13 @@
 // defining Arduino input and output pins
 #define buzzer 5
 
-#define r_led A3
-#define r_in 2
-#define r_cup 3
+#define r_led A2
+#define r_in 4
+#define r_cup 6
 
-#define l_led A2
-#define l_in 4
-#define l_cup 6
+#define l_led A3
+#define l_in 2
+#define l_cup 3
 
 #define earth 7
 #define earth_led A0
@@ -16,10 +16,13 @@
 int r_val;
 int previous_r_val = HIGH;
 int r_cup_val;
+bool r_cup_touch = false;
+bool cup_touch = false;
 
 int l_val;
 int previous_l_val = HIGH;
 int l_cup_val;
+bool l_cup_touch = false;
 
 int earth_val;
 
@@ -31,6 +34,7 @@ long prt = 0;
 long plt = 0;
 long pet = 0;
 
+bool m = false;
 bool l_first = false;
 bool r_first = false;
 
@@ -39,6 +43,7 @@ int l_points = 0;
 int previous_r_points = 0;
 int previous_l_points = 0;
 
+int trigger;
 
 long threshold = 40;
 
@@ -54,7 +59,7 @@ void setup() {
   pinMode(l_cup, INPUT_PULLUP);
   pinMode(earth, INPUT_PULLUP);
   pinMode(earth_led, OUTPUT);
-  Serial.begin(9600);
+  //Serial.begin(9600);
 }
 
 void loop() {
@@ -69,18 +74,30 @@ void loop() {
 
   earth_val = digitalRead(earth);
 
+  //if(r_cup_val == LOW && cup_touch == false){
+  //  cup_touch = true;
+  //}
+
+  //if(l_cup_val == LOW && cup_touch == false){
+  //  cup_touch = true;
+  //}
+
 // right side first
   // if right epee button has been pressed (r_val == LOW means right epee button has been pressed) and it wasn't pressed before, and left epee button has not been pressed (!l_first means l_first = false)
   if(r_val == LOW && previous_r_val == HIGH && !l_first){
+    cup_touch = false;
+    m = true;
+    trigger = 1;
     // check give points if cup or piste are not receiving current (in this case, HIGH, means the l_cup and earth aren't receiving current)
     if(l_cup_val == HIGH && earth_val == HIGH){
       // give points to right player
       r_points++;
+      trigger = 2;
       // previous right timer (prt) equals current time. This will "reset" our r_timer, which is defined some lines bellow as: r_timer = ct - prt. We will use this timer to see if there was a double touch
       prt = ct;
       // turn on right LED and buzzer
-      digitalWrite(r_led, HIGH);
-      digitalWrite(buzzer, HIGH);
+      //digitalWrite(r_led, HIGH);
+      //digitalWrite(buzzer, HIGH);
       // right player was the first to touch
       r_first = true;
       // previous right value is LOW (meaning that the button was pressed). This is done to avoid this if statement from ruinning indefinetly
@@ -90,29 +107,44 @@ void loop() {
 
   // since prt = ct, the timer will be restarted and will count from 0 to 40 milliseconds (the time for the double touch to be awarded)
   r_timer = ct - prt;
-
   // while the r_timer is not above 40 ms, and left player hasn't touched
   if(!l_first && r_timer < threshold){
-    digitalWrite(r_led, HIGH);
+    trigger = 4;
+    //digitalWrite(r_led, HIGH);
     // if left player's button has been pressed
     if(l_val == LOW){
+      trigger = 5;
       // and it wasn't cup or ground
       if(r_cup_val == HIGH && earth_val == HIGH){
+        trigger = 6;
       // award points to left player
       l_points++;
-      digitalWrite(l_led, HIGH);
+      //digitalWrite(l_led, HIGH);
       }
     }
+    if(r_cup_val == LOW && cup_touch == false){
+      trigger = 7;
+    cup_touch = true;
+  }
+
+  if(l_cup_val == LOW && cup_touch == false){
+    trigger = 8;
+    cup_touch = true;
+  }
   }
 
 // left side first
   
   if(l_val == LOW && previous_l_val == HIGH && r_first == false){
+    trigger = 9;
+    cup_touch = false;
+    m = true;
     if(r_cup_val == 1 && earth_val == 1){
+      trigger = 10;
       l_points++;
       plt = ct;
-      digitalWrite(l_led, HIGH);
-      digitalWrite(buzzer, HIGH);
+      //digitalWrite(l_led, HIGH);
+      //digitalWrite(buzzer, HIGH);
       l_first = true;
       previous_l_val = LOW;
     }
@@ -121,21 +153,35 @@ void loop() {
   l_timer = ct - plt;
   
   if(l_timer < threshold){
-    digitalWrite(l_led, HIGH);
+    trigger = 11;
+    //digitalWrite(l_led, HIGH);
     if(r_val == LOW){
+      trigger = 12;
       if(l_cup_val == HIGH && earth_val == HIGH){
+        trigger = 13;
       r_points++;
-      digitalWrite(r_led, HIGH);
+      //digitalWrite(r_led, HIGH);
       }
     }
+    if(r_cup_val == LOW && cup_touch == false){
+      trigger = 14;
+    cup_touch = true;
+  }
+
+  if(l_cup_val == LOW && cup_touch == false){
+    trigger = 15;
+    cup_touch = true;
+  }
   }
   
   earth_timer = ct - pet;
 
   // after right and left coutdown timers exceed the double touch time threshold, turn on the scoreboard lights
   if(r_timer > threshold && l_timer > threshold){
+    trigger = 16;
     // if both touch, turn both lights
-    if(r_points != previous_r_points && l_points != previous_l_points){
+    if(r_points != previous_r_points && l_points != previous_l_points && cup_touch == false && m == true){
+      trigger = 17;
       digitalWrite(r_led, HIGH);
       digitalWrite(l_led, HIGH);
       digitalWrite(buzzer, HIGH);
@@ -149,10 +195,13 @@ void loop() {
       l_first = false;
       previous_r_val = HIGH;
       previous_l_val = HIGH;
+      cup_touch = false;
+      m = false;
       delay(1000);
     }
     // if only right side touches, turn only right LEDs
-    else if(r_points != previous_r_points && l_points == previous_l_points){
+    else if(r_points != previous_r_points && l_points == previous_l_points && cup_touch == false && m == true){
+      trigger = 18;
       digitalWrite(r_led, HIGH);
       digitalWrite(buzzer, HIGH);
       delay(1500);
@@ -165,10 +214,13 @@ void loop() {
       l_first = false;
       previous_r_val = HIGH;
       previous_l_val = HIGH;
+      cup_touch = false;
+      m = false;
       delay(1000);
     }
     // if only left side touches, turn only left LEDs
-    else if(r_points == previous_r_points && l_points != previous_l_points){
+    else if(r_points == previous_r_points && l_points != previous_l_points && cup_touch == false && m == true){
+      trigger = 19;
       digitalWrite(l_led, HIGH);
       digitalWrite(buzzer, HIGH);
       delay(1500);
@@ -180,17 +232,30 @@ void loop() {
       l_first = false;
       previous_r_val = HIGH;
       previous_l_val = HIGH;
+      cup_touch = false;
+      m = false;
       delay(1000);
     }
-
+    
     // if no one touches, lights and buzzer stay off
     else{
+      trigger = 20;
     digitalWrite(r_led, LOW);
     digitalWrite(l_led, LOW);
     digitalWrite(buzzer, LOW);
+    r_first = false;
+      l_first = false;
+      previous_r_val = HIGH;
+      previous_l_val = HIGH;
+      cup_touch = false;
+      m = false;
   }
   }
-
+//if(r_timer > threshold && l_timer > threshold){
+//      cup_touch = true;
+ //     trigger = 21;
+  //  }
+  
   // cup grounding LEDs
   if(r_cup_val == LOW || l_cup_val == LOW){
     digitalWrite(earth_led, HIGH);
@@ -200,9 +265,11 @@ void loop() {
     digitalWrite(earth_led, LOW);
   }
 
-
+  //Serial.print("l_cup:  "); Serial.println(cup_touch);
+  //Serial.print("trigger:  "); Serial.println(trigger);
+  //Serial.print("r_val:  "); Serial.println(previous_r_val);
   // uncomment for debugging
-  
+  /*
   Serial.print("right:  "); Serial.print(r_val); Serial.print("     touch?   "); Serial.println(previous_r_val);
   
   Serial.print("left:   "); Serial.print(l_points); Serial.print("     touch?   "); Serial.println(l_first);
@@ -214,5 +281,5 @@ void loop() {
   Serial.print("r_timer:  "); Serial.println(r_timer);
   Serial.print("l_timer:  "); Serial.println(l_timer);
   Serial.println(""); Serial.println(""); Serial.println(""); Serial.println("");
-  
+  */
 }
